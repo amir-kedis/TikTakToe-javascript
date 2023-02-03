8c6aee1ecc7a5adb6158d3399ae144cb0474f140
amir-kedis <amir.kedis@gmail.com>
2023-02-03 09:27:05 +0200

    🔨 setting up JS structure and modules

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * This Module is responsible of controlling
+ * and storing the game board
+ */
+const gameBoard = (() => {
+
+})();
+
+/**
+ * This Module is responsible of rendering elements to the DOM
+ * and getting inputs
+ */
+const displayController = (() => {
+
+})();
+
+/**
+ * This Factory is responsible of creating player objs
+ * which control player info
+ */
+const createPlayer = () => {
+
+};
+
+/**
+ * this module is responsible of making other modules 
+ * comminute and managing the game on a high value
+ */
+const game = (() => {
+
+})();
